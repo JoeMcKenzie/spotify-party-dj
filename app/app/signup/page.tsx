8ce74722158868dcc1123e3a160c7c9f4191a1ef
linju <linjustin167@gmail.com>
2026-05-01 19:09:19 -0500

    Changed COLOR styles for sign up page and login in page.

--- app/app/signup/page.tsx
+++ app/app/signup/page.tsx
@@ -50,7 +50,7 @@ export default function SignUpPage() {
 
         {/* Card */}
         <div className="bg-white p-8 rounded-2xl shadow-md">
-          <h1 className="text-3xl font-bold text-center">
+          <h1 className="text-3xl font-bold text-center text-gray-900">
             Sign Up
           </h1>
           <p className="mt-2 text-sm text-gray-500 text-center">
@@ -59,19 +59,19 @@ export default function SignUpPage() {
 
           <form onSubmit={handleSubmit} className="mt-6 space-y-4">
             <div>
-              <label className="block mb-1 font-medium">Username</label>
+              <label className="block mb-1 font-medium text-gray-500">Username</label>
               <input
-                className="w-full border rounded px-3 py-2"
+                className="w-full border border-gray-200 rounded px-3 py-2 text-gray-500"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
               />
             </div>
 
             <div>
-              <label className="block mb-1 font-medium">Password</label>
+              <label className="block mb-1 font-medium text-gray-500">Password</label>
               <input
                 type="password"
-                className="w-full border rounded px-3 py-2"
+                className="w-full border border-gray-200 rounded px-3 py-2 text-gray-500"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
